3a9304a5193921c02ae23a30db861c9fd352bc70
Jorge Ceja <jorgeceja123@gmail.com>
2018-01-26 21:46:30 -0800

    [feature] add helmet middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const { GraphQLServer } = require('graphql-yoga');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const config = require('./config/config');
@@ -30,6 +31,9 @@ const server = new GraphQLServer({
   resolvers,
   context: req => ({ ...req })
 });
+
+// HTTP security middleware
+server.express.use(helmet());
 
 // only if you're behind a reverse proxy
 // (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
